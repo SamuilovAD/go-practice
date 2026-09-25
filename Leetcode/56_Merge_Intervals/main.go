3c70ab715b9a741e77743864dc91020658b6d6a2
Alexandr Samuilov <mogilevpa@yandex.ru>
2026-09-25 11:00:42 +0300

    2026-09-25

---
+++ Leetcode/56_Merge_Intervals/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	intervals := [][]int{
+		{1, 3},
+		{2, 6},
+		{8, 10},
+		{15, 18},
+	}
+	fmt.Printf("Merged by Sort: %v", mergeSort(intervals))
+}
+
+// Time: O (N log N) Space (N)
+func mergeSort(intervals [][]int) [][]int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+	merged := make([][]int, 0)
+	for _, interval := range intervals {
+		if len(merged) == 0 || merged[len(merged)-1][1] < interval[0] {
+			merged = append(merged, interval)
+		} else {
+			merged[len(merged)-1][1] = max(merged[len(merged)-1][1], interval[1])
+		}
+
+	}
+
+	return merged
+}
